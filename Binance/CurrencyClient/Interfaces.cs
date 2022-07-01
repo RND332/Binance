74e10d21947b6c5765ec1d58638dde976724d2da
Eureqs Club <eureq631@gmail.com>
2022-07-01 17:01:36 +0300

    Add interfaces for Deserialize

---
+++ Binance/CurrencyClient/Interfaces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binance
+{
+    /// <summary>
+    /// Used for Deserialize responce from Currency
+    /// </summary>
+    public interface ICurrencyPrice
+    {
+        public long timestamp { get; set; }
+        public List<List<double>> asks { get; set; }
+        public List<List<double>> bids { get; set; }
+    }
+    /// <summary>
+    /// Used for answering user request
+    /// </summary>
+    public interface IPrices
+    {
+        public long timestamp { get; set; }
+        public double asks { get; set; }
+        public double bids { get; set; }
+    }
+}
